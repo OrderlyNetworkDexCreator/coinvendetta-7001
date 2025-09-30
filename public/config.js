2ccb6c721cfab0a57aca4f7cdba5557b44fb8703
Mario Reder <mario.reder@pm.me>
2025-09-30 09:58:59 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -17,7 +17,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Get your revenge from the crypto markets",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Portfolio,Leaderboard,Rewards,Markets",
+  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_CAMPAIGNS": "true",
   "VITE_TELEGRAM_URL": "",
